e7787a29da74bd0a9abf6bbe437c534df2cbc0e1
Mynul Islam <mynul.sakil@gmail.com>
2025-03-10 23:43:06 +0600

    adding shadow v-50

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -3,7 +3,7 @@ import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
     return (
-        <View style={styles.screen}>
+        <View style={styles.inputContainer}>
             <TextInput />
             <PrimaryButton>Reset</PrimaryButton>
             <PrimaryButton>Confirm</PrimaryButton>
@@ -11,6 +11,18 @@ function StartGameScreen() {
     )
 }
 const styles = StyleSheet.create({
-  
+  inputContainer:{
+    padding:16,
+    marginTop:100,
+    marginHorizontal:24,
+    backgroundColor:'#72063C',
+    borderRadius:8,
+    elevation:4, // shadow on android,
+    shadowColor:'black', // shadow on ios
+    shadowOffset:{width:0,height:2},
+    shadowOpacity:0.26,
+    shadowRadius:8,
+
+  }
 });
 export default StartGameScreen;
