61f110a233a0d39d45938d752129b9a3a310c0b4
Mynul Islam <mynul.sakil@gmail.com>
2025-03-11 07:05:24 +0600

    v-54

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -6,34 +6,49 @@ function StartGameScreen() {
         <View style={styles.inputContainer}>
             <TextInput style={styles.numberInput} maxLength={2} keyboardType="number-pad"
                 autoCapitalize="none" autoCorrect={false} />
-            <PrimaryButton>Reset</PrimaryButton>
-            <PrimaryButton>Confirm</PrimaryButton>
+            <View style={styles.buttonsContainer}>
+                <View style={styles.buttonContainer}>
+
+                    <PrimaryButton>Reset</PrimaryButton>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton>Confirm</PrimaryButton>
+                </View>
+            </View>
         </View>
     )
 }
 const styles = StyleSheet.create({
-  inputContainer:{
-    padding:16,
-    marginTop:100,
-    marginHorizontal:24,
-    backgroundColor:'#4e0329',
-    borderRadius:8,
-    elevation:4, // shadow on android,
-    shadowColor:'black', // shadow on ios
-    shadowOffset:{width:0,height:2},
-    shadowOpacity:0.26,
-    shadowRadius:8,
-},
-numberInput:{
-    height:50,
-    width:50,
-    fontSize:32,
-    borderBottomColor:'#ddb52f',
-    borderBottomWidth:2,
-    color:'#ddb52f',
-    marginVertical:8,
-    fontWeight:'bold',
-    textAlign:'center',
-},
+    inputContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 16,
+        marginTop: 100,
+        marginHorizontal: 24,
+        backgroundColor: '#4e0329',
+        borderRadius: 8,
+        elevation: 4, // shadow on android,
+        shadowColor: 'black', // shadow on ios
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.26,
+        shadowRadius: 8,
+    },
+    numberInput: {
+        height: 50,
+        width: 50,
+        fontSize: 32,
+        borderBottomColor: '#ddb52f',
+        borderBottomWidth: 2,
+        color: '#ddb52f',
+        marginVertical: 8,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    buttonsContainer: {
+        flexDirection: 'row',
+    },
+    buttonContainer:{
+        flex:1,
+    }
 });
 export default StartGameScreen;
