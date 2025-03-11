fa793d8ff22a370bb2f75ad2e61d5d6962bbdaf4
Mynul Islam <mynul.sakil@gmail.com>
2025-03-11 07:08:18 +0600

    55

--- App.js
+++ App.js
@@ -4,8 +4,16 @@ import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
+    <View style={styles.rootScreen}>
+
     <StartGameScreen />
+    </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootScreen:{
+    flex:1,
+    backgroundColor:'#ddb52f',
+  }
+});
