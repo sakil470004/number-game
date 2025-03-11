99aca8094b4a7f6c82ea1594e12053574764eb35
Mynul Islam <mynul.sakil@gmail.com>
2025-03-11 07:20:10 +0600

    added liniear gradient 56

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
-    <View style={styles.rootScreen}>
-
-    <StartGameScreen />
-    </View>
+    <LinearGradient colors={['#99b52f', '#e9e9e9', '#3b5998',]} style={styles.rootScreen}>
+      <ImageBackground>
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
-  rootScreen:{
-    flex:1,
-    backgroundColor:'#ddb52f',
+  rootScreen: {
+    flex: 1,
   }
 });
