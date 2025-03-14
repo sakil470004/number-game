8ddd225762b9988ba5160b823eae9210fb28acae
Mynul Islam <mynul.sakil@gmail.com>
2025-03-14 10:19:49 +0600

    70 icons

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -5,6 +5,7 @@ import NumberContainer from "../components/game/NumberContainer";
 import PrimaryButton from "../components/ui/PrimaryButton";
 import Card from "../components/ui/Card";
 import InstructionText from "../components/ui/InstructionText";
+import { Ionicons } from '@expo/vector-icons';
 
 function generateRandomBetween(min, max, exclude) {
   const rndNum = Math.floor(Math.random() * (max - min)) + min;
@@ -51,10 +52,14 @@ function GameScreen(props) {
         <InstructionText style={styles.instructionText}>Higher Or Lower</InstructionText>
         <View style={styles.buttonsContainer} >
           <View style={styles.buttonContainer}>
-<PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>-</PrimaryButton>
+            <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>
+              <Ionicons name="remove" size={24} color="white" />
+            </PrimaryButton>
           </View>
           <View style={styles.buttonContainer}>
-            <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>+</PrimaryButton>
+            <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>
+            <Ionicons name="add" size={24} color="white" />
+            </PrimaryButton>
           </View>
         </View>
       </Card>
@@ -68,8 +73,8 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 12,
   },
-  instructionText:{
-    marginBottom : 24,
+  instructionText: {
+    marginBottom: 24,
   },
   buttonsContainer: {
     flexDirection: 'row',
